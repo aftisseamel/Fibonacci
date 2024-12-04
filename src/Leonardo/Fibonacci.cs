1a1dc7d4de37d3b7c0b0f544411843fde62b1baf
aftisseamel <asensioamel@gmail.com>
2024-12-04 16:54:36 +0100

    leonardo updated database

--- src/Leonardo/Fibonacci.cs
+++ src/Leonardo/Fibonacci.cs
@@ -1,37 +1,54 @@
-﻿namespace Leonardo;
+﻿using Microsoft.EntityFrameworkCore;
 
-public record FibonacciResult(int Input, int Result);
+namespace Leonardo;
+
+public record FibonacciResult(int Input, long Result);
 
 public static class Fibonacci
 {
-    public static int Run(int i)
+    private static int Run(int i)
     {
         if (i <= 2)
+        {
             return 1;
+        }
 
         return Run(i - 1) + Run(i - 2);
     }
+
     public static async Task<List<FibonacciResult>> RunAsync(string[] strings)
     {
         var tasks = new List<Task<FibonacciResult>>();
+        var context = new FibonacciDataContext();
+
         foreach (var input in strings)
         {
-            var int32 = Convert.ToInt32(input);
-            var r = Task.Run(() =>
+            var i = int.Parse(input);
+            var result = await context.TFibonaccis.Where(f => f.FibInput == i).FirstOrDefaultAsync();
+
+            if (result != null)
             {
-                var result = Fibonacci.Run(int32);
-                return new FibonacciResult(int32, result);
-            });
-            tasks.Add(r);
+                tasks.Add(Task.FromResult(new FibonacciResult(i, result.FibOutput)));
+            }
+            else
+            {
+                tasks.Add(Task.Run(() => new FibonacciResult(i, Run(i))));
+            }
         }
 
         var results = new List<FibonacciResult>();
         foreach (var task in tasks)
         {
-            var r = await task;
-            results.Add(r);
+            results.Add(await task);
+
+            context.TFibonaccis.Add(new TFibonacci
+            {
+                FibInput = results.Last().Input,
+                FibOutput = results.Last().Result
+            });
         }
 
+        await context.SaveChangesAsync();
         return results;
     }
 }
